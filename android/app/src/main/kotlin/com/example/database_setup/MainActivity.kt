713576c6cd6ba044c55324d582e6e55624fa76ea
Muhammad Usman <usman5194999@gmail.com>
2024-12-25 20:19:32 +0500

    Sending Data To Firebase Using Flutter

---
+++ android/app/src/main/kotlin/com/example/database_setup/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.database_setup
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SendDatatoDB(),
+    );
+  }
+}
+
+class SendDatatoDB extends StatefulWidget {
+  @override
+  _SendDatatoDB createState() => _SendDatatoDB();
+}
+
+class _SendDatatoDB extends State<SendDatatoDB> {
+  final TextEditingController _controller = TextEditingController();
+
+  final database_ref = FirebaseDatabase.instanceFor(
+          app: Firebase.app(),
+          databaseURL:
+              "https://database-practice-5fa22-default-rtdb.firebaseio.com/")
+      .ref();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Sending Data To DB"),
+        backgroundColor: Colors.amber,
+        foregroundColor: Colors.black87,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 70),
+            TextField(
+              controller: _controller,
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(), hintText: "Enter Name"),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          database_ref
+              .child('Names')
+              .push()
+              .set({'Name': _controller.text.toString()})
+              .then((_) {})
+              .onError((error, stackTrace) {
+                print("Error: $error");
+              });
+        },
+        tooltip: "Add Data To Database",
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
